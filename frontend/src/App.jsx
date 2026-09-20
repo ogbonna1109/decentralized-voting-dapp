24c2c4b64f687529625fbdfc02459eb66a8e6453
ogbonna1109 <ogbonna1109@gmail.com>
2026-09-20 02:50:53 +0100

    feat: build voting dashboard, voting, and results pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import VotePage from "./pages/VotePage";
-
+import ResultsPage from "./pages/ResultsPage";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/vote" element={<VotePage />} />
+        <Route path="/results" element={<ResultsPage />} />
       </Routes>
     </BrowserRouter>
   );
